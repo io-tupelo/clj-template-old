a3898abcb79f24ce783a70fb46cec1169f4e8ce2
Alan Thompson <cloojure@gmail.com>
2020-02-01 08:34:10 -0800

    misc

--- src/clj/demo/core.clj
+++ src/clj/demo/core.clj
@@ -1,8 +1,14 @@
 (ns demo.core
   (:use tupelo.core)
   (:require
-    [clojure.string :as str])
+    [clojure.string :as str]
+    [schema.core :as s])
   (:gen-class))
+
+(s/defn add2 :- s/Num
+  [x :- s/Num
+   y :- s/Num]
+  (+ x y))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/clj/tst/demo/core.clj
+++ test/clj/tst/demo/core.clj
@@ -7,7 +7,10 @@
 (dotest
   (is= 5 (+ 2 3))
   (isnt= 9 (+ 2 3))
-  (throws? (/ 1 0))
+  (throws? (/ 1 0)) ; verify that an illegal operation throws an exception
+
+  (is= 3 (add2 1 2))
+  (throws? (add2 1 "two")) ; Prismatic Schema will throw since "two" is not a number
   )
 
 (dotest
