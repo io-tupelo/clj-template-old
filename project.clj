7d04ccb1a115603c4dae155909935a2e0ab501a1
Alan Thompson <cloojure@gmail.com>
2021-05-04 21:24:40 -0700

    misc

--- project.clj
+++ project.clj
@@ -5,16 +5,16 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [cambium/cambium.codec-simple "1.0.0"]
-                 [cambium/cambium.core         "1.0.0"]
+                 [cambium/cambium.core         "1.1.0"]
                  [cambium/cambium.logback.core "0.4.4"]
                  [com.rpl/specter "1.1.3"]
-                 [org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojure "1.10.3"]
                  [prismatic/schema "1.1.12"]
-                 [tupelo "20.12.01"]
+                 [tupelo "21.04.13"]
                  ]
   :plugins [
     [com.jakemccrary/lein-test-refresh "0.24.1"]
-    [lein-ancient "0.6.15"]
+    [lein-ancient "0.7.0"]
     ]
 
   :global-vars {*warn-on-reflection* false}
